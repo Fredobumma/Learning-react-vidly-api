5f3b66f47b26d397d5c74b471dddece584a5c710
Manfred Obumma <fredobumma@gmail.com>
2023-03-05 15:05:00 +0100

    --bug: bugfix

--- routes/movies.js
+++ routes/movies.js
@@ -9,18 +9,42 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  try {
-    const movies = await Movie.find().select("-__v").sort("name");
-    res.send(movies);
-  } catch (error) {
-    return res.status(400).send("Bad Request.");
-  }
+  const movies = await Movie.find().select("-__v").sort("name");
+  res.send(movies);
 });
 
-router.post("/", async (req, res) => {
-  try {
-    const genre = await Genre.findById(req.body.genreId);
-    const movie = new Movie({
+router.post("/", [auth], async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send("Invalid genre.");
+
+  const movie = new Movie({
+    title: req.body.title,
+    genre: {
+      _id: genre._id,
+      name: genre.name,
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate,
+    publishDate: moment().toJSON(),
+  });
+  await movie.save();
+
+  res.send(movie);
+});
+
+router.put("/:id", [auth], async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send("Invalid genre.");
+
+  const movie = await Movie.findByIdAndUpdate(
+    req.params.id,
+    {
       title: req.body.title,
       genre: {
         _id: genre._id,
@@ -28,66 +52,32 @@ router.post("/", async (req, res) => {
       },
       numberInStock: req.body.numberInStock,
       dailyRentalRate: req.body.dailyRentalRate,
-      publishDate: moment().toJSON(),
-    });
-    await movie.save();
+    },
+    { new: true }
+  );
 
-    res.send(movie);
-  } catch (error) {
-    return res.status(400).send("Invalid genre.");
-  }
-});
-
-router.put("/:id", async (req, res) => {
-  // const { error } = validate(req.body);
-  // if (error) return res.status(400).send(error.details[0].message);
-
-  try {
-    let genre;
-    try {
-      genre = await Genre.findById(req.body.genreId);
-    } catch (error) {
-      return res.status(400).send("Invalid genre.");
-    }
-    const movie = await Movie.findByIdAndUpdate(
-      req.params.id,
-      {
-        title: req.body.title,
-        genre: {
-          _id: genre._id,
-          name: genre.name,
-        },
-        numberInStock: req.body.numberInStock,
-        dailyRentalRate: req.body.dailyRentalRate,
-      },
-      { new: true }
-    );
-    res.send(movie);
-  } catch (error) {
+  if (!movie)
     return res.status(404).send("The movie with the given ID was not found.");
-  }
-});
 
-router.delete("/:id", async (req, res) => {
-  try {
-    const movie = await Movie.findByIdAndRemove(req.params.id);
-    res.send(movie);
-  } catch (error) {
-    return res.status(404).send("The movie with the given ID was not found.");
-  }
-});
-
-router.get("/:id", async (req, res) => {
-  const movie = await Movie.findById(req.params.id).select("-__v");
   res.send(movie);
-  console.log(movie);
-  // try {
-  //   const movie = await Movie.findById(req.params.id).select("-__v");
-  //   if (movie) res.send(movie);
-  //   else res.send("Movie not found");
-  // } catch (error) {
-  //   return res.status(404).send("The movie with the given ID was not found.");
-  // }
+});
+
+router.delete("/:id", [auth, admin], async (req, res) => {
+  const movie = await Movie.findByIdAndRemove(req.params.id);
+
+  if (!movie)
+    return res.status(404).send("The movie with the given ID was not found.");
+
+  res.send(movie);
+});
+
+router.get("/:id", validateObjectId, async (req, res) => {
+  const movie = await Movie.findById(req.params.id);
+
+  if (!movie)
+    return res.status(404).send("The movie with the given ID was not found.");
+
+  res.send(movie);
 });
 
 module.exports = router;
